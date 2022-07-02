13cf8e02c73c6884ba54a399fa7fdf7277c90799
PriyankaGire <priyagire94@gmail.com>
2022-07-02 16:00:42 +0530

    update second

--- Calculator.java
+++ Calculator.java
@@ -1,14 +1,13 @@
-import java.util.Scanner;
+
 
 public class Calculator {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter first number:");
-		int n1=sc.nextInt();
-		System.out.println("Enter second number:");
-		int n2=sc.nextInt();
+		
+		int n1=10;
+		
+		int n2=20;
 		
 		System.out.println("Addition= "+ n1+n2);
 		System.out.println("Substraction= "+ (n1 - n2));
